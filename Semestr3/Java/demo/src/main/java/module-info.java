7a8ef5e8d99e0414aed36eb3a2858a238275b226
slxshxr17 <szczepaniakdominik5@gmail.com>
2024-01-26 20:34:24 +0100

    Update semestr3

--- Semestr3/Java/demo/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module lista10.demo {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens lista10.demo to javafx.fxml;
-    exports lista10.demo;
-}
--- Semestr3/Java/test/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module lista10.test {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens lista10.test to javafx.fxml;
-    exports lista10.test;
-}
